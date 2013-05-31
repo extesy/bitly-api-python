2fcba3b70c742910287bd05f4ce6f5c28a2745be
Oleg Anashkin <oleg.anashkin@gmail.com>
2013-05-30 21:42:23 -0700

    change package name

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='bitly_api3',
       url='https://github.com/bitly/bitly-api-python',
       download_url="https://bitly-downloads.s3.amazonaws.com/bitly_api/bitly_api-%s.tar.gz" % version,
       license='MIT License',
-      packages=['bitly_api3'],
+      packages=['bitly_api'],
       include_package_data=True,
       zip_safe=True,
       )
