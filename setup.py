dadb99b2f58ddcf8cb0ab3f2dbfe934296f0a935
James Socol <james@bit.ly>
2013-08-01 15:27:18 -0400

    Version 0.3

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-version = '0.2'
+version = '0.3'
 
-setup(name='bitly_api3',
+setup(name='bitly_api',
       version=version,
       description="An API for bit.ly",
       long_description=open("./README.md", "r").read(),
@@ -15,15 +15,14 @@ setup(name='bitly_api3',
           "Programming Language :: Python",
           "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
           "Topic :: Utilities",
-          "License :: OSI Approved :: MIT License",
+          "License :: OSI Approved :: Apache Software License",
           ],
       keywords='bitly bit.ly',
       author='Jehiah Czebotar',
       author_email='jehiah@gmail.com',
       url='https://github.com/bitly/bitly-api-python',
-      download_url="https://bitly-downloads.s3.amazonaws.com/bitly_api/bitly_api-%s.tar.gz" % version,
-      license='MIT License',
-      packages=['bitly_api3'],
+      license='Apache Software License',
+      packages=['bitly_api'],
       include_package_data=True,
       zip_safe=True,
       )
